0b2093073e8cce0b593555db8c792cb74c33e51f
FATHIMA JUBIN PK <fathimajubin2001@gmail.com>
2026-01-22 16:39:20 +0530

    Add files via upload

---
+++ Day 1/HelloWinForms/HelloWinForms/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace HelloWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblMessage = new Label();
+            txtName = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            lblMessage.AutoSize = true;
+            lblMessage.Location = new Point(355, 66);
+            lblMessage.Name = "lblMessage";
+            lblMessage.Size = new Size(38, 15);
+            lblMessage.TabIndex = 0;
+            lblMessage.Text = "label1";
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(327, 161);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(100, 23);
+            txtName.TabIndex = 1;
+            txtName.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(352, 260);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(txtName);
+            Controls.Add(lblMessage);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblMessage;
+        private TextBox txtName;
+        private Button button1;
+    }
+}
